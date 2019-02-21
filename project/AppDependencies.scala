9269d0f9892b0bc1f8b3ece66c07b139324e1828
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2019-02-21 15:29:12 +0000

    CDD-1889: changed from playReactiveMongo to simpleReactiveMongo & updated some other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,15 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcTestVersion = "3.4.0-play-25"
+  private val hmrcTestVersion = "3.5.0-play-25"
   private val scalaTestVersion = "3.0.5"
   private val scalatestplusVersion = "2.0.1"
-  private val mockitoVersion = "2.23.4"
-  private val wireMockVersion = "2.20.0"
+  private val mockitoVersion = "2.24.5"
+  private val wireMockVersion = "2.21.0"
   private val customsApiCommonVersion = "1.36.0"
+  private val simpleReactiveMongoVersion = "7.12.0-play-25"
+  private val reactiveMongoTestVersion = "4.7.0-play-25"
   private val testScope = "test,it"
-  private val playReactivemongoVersion = "6.2.0"
 
   val hmrcTest = "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % testScope
 
@@ -23,10 +24,9 @@ object AppDependencies {
 
   val customsApiCommon = "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion
 
-  val playReactiveMongo = "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion
+  val simpleReactiveMongo = "uk.gov.hmrc" %% "simple-reactivemongo" % simpleReactiveMongoVersion
 
-  val reactiveMongoTest = "uk.gov.hmrc" %% "reactivemongo-test" % "3.1.0" % testScope
+  val reactiveMongoTest = "uk.gov.hmrc" %% "reactivemongo-test" % reactiveMongoTestVersion % testScope
 
   val customsApiCommonTests = "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion % testScope classifier "tests"
-
 }
