c162796a3eeaa98a1be44460a88c6fe41b064e75
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2019-03-13 14:36:23 +0000

    CDD-2142: Bumped up scalatest version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val hmrcTestVersion = "3.6.0-play-25"
-  private val scalaTestVersion = "3.0.5"
+  private val scalaTestVersion = "3.0.6"
   private val scalatestplusVersion = "2.0.1"
   private val mockitoVersion = "2.25.0"
   private val wireMockVersion = "2.21.0"
