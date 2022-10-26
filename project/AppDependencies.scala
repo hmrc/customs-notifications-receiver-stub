1ea25b0d66ed0af8b954d5c1ff9b01332d57ab70
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2022-10-26 08:35:31 +0100

    Play libraries upgraded

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,8 +8,8 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion,
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "8.0.0-play-28",
-    "uk.gov.hmrc" %% "work-item-repo" % "8.1.0-play-28",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "8.1.0-play-28",
+    "uk.gov.hmrc" %% "work-item-repo" % "8.2.0-play-28",
     "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full,
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full)
   )
@@ -20,7 +20,7 @@ object AppDependencies {
     "com.github.tomakehurst" % "wiremock-standalone" % "2.27.1" % testScope,
     "org.scalatestplus" %% "mockito-3-4" % "3.2.9.0" % testScope,
 
-    "uk.gov.hmrc" %% "reactivemongo-test" % "5.0.0-play-28" % testScope,
+    "uk.gov.hmrc" %% "reactivemongo-test" % "5.1.0-play-28" % testScope,
     "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % testScope
   )
 }
