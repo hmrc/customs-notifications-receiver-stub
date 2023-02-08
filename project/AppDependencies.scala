82ed370d8cec8ceaebe7a4490fa7961d8a4e1089
Edward Oscar Taylor <48755019+ed1286@users.noreply.github.com>
2023-02-08 20:48:26 +0000

    DCWL-538

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
 
   private val testScope = "test,it"
+  private lazy val mongoDbVersion = "0.73.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"                                %% "customs-api-common"   % "1.57.0",
-    "uk.gov.hmrc"                                %% "simple-reactivemongo" % "8.1.0-play-28",
-    "uk.gov.hmrc"                                %% "work-item-repo"       % "8.2.0-play-28",
-    "com.github.ghik"                            %  "silencer-lib"         % "1.7.11"          % Provided cross CrossVersion.full,
+    "uk.gov.hmrc"                    %% "customs-api-common"   % "1.57.0",
+    "uk.gov.hmrc.mongo"              %% "hmrc-mongo-play-28"   % mongoDbVersion,
+    "com.github.ghik"                %  "silencer-lib"         % "1.7.11"          % Provided cross CrossVersion.full,
     compilerPlugin("com.github.ghik" %  "silencer-plugin"      % "1.7.11"                     cross CrossVersion.full)
   )
 
@@ -17,7 +17,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "customs-api-common"       % "1.57.0"        % testScope classifier "tests",
     "com.github.tomakehurst" %  "wiremock-standalone"      % "2.27.2"        % testScope,
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"      % testScope,
-    "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.1.0-play-28" % testScope,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % mongoDbVersion % testScope,
     "com.vladsch.flexmark"   %  "flexmark-all"             % "0.35.10"       % testScope
   )
 }
