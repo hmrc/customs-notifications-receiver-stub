6490aab3cf8de23ef17b5c0e848bd33ae805fed4
Edward Oscar Taylor <48755019+ed1286@users.noreply.github.com>
2023-02-09 18:15:42 +0000

    DCWL-538

--- test/util/TestData.scala
+++ test/util/TestData.scala
@@ -39,31 +39,30 @@ object TestData {
   val header4: Header = Header(name = "testHeader4", value = "value4")
   val header5: Header = Header(name = "testHeader5", value = "value5")
   val header6: Header = Header(name = "testHeader6", value = "value6")
-  //TODO revert to seq?
   val headers1: List[Header] = List(header1, header2)
   val headers2: List[Header] = List(header3, header4)
   val headers3: List[Header] = List(header5, header6)
-  lazy val objectId1: ObjectId = new ObjectId
-  lazy val objectId2: ObjectId = new ObjectId
-  lazy val objectId3: ObjectId = new ObjectId
-  lazy val objectId4: ObjectId = new ObjectId
-  lazy val objectId5: ObjectId = new ObjectId
-  lazy val objectId6: ObjectId = new ObjectId
-  lazy val authHeaderToken: String = "testAuthHeaderToken"
-  lazy val xmlPayload: String = "testXmlPayload"
-  lazy val notificationRequest1: NotificationRequest = NotificationRequest(
+  val objectId1: ObjectId = new ObjectId("63e527c7af9eb2415b7d4d36")
+  val objectId2: ObjectId = new ObjectId("63e52773ba3698660c6e6020")
+  val objectId3: ObjectId = new ObjectId("63e5299c0b3c451a390c3ab2")
+  val objectId4: ObjectId = new ObjectId("63e529c26bdb5560c924838a")
+  val objectId5: ObjectId = new ObjectId("63e529e0968d6b7c482e44a6")
+  val objectId6: ObjectId = new ObjectId("63e52a05ef67a61170aabc04")
+  val authHeaderToken: String = "testAuthHeaderToken"
+  val xmlPayload: String = "testXmlPayload"
+  val notificationRequest1: NotificationRequest = NotificationRequest(
     csId = csId1,
     conversationId = conversationId1,
     authHeaderToken = authHeaderToken,
     outboundCallHeaders = headers1,
     xmlPayload = xmlPayload)
-  lazy val notificationRequest2: NotificationRequest = NotificationRequest(
+  val notificationRequest2: NotificationRequest = NotificationRequest(
     csId = csId2,
     conversationId = conversationId2,
     authHeaderToken = authHeaderToken,
     outboundCallHeaders = headers2,
     xmlPayload = xmlPayload)
-  lazy val notificationRequest3: NotificationRequest = NotificationRequest(
+  val notificationRequest3: NotificationRequest = NotificationRequest(
     csId = csId3,
     conversationId = conversationId3,
     authHeaderToken = authHeaderToken,
@@ -71,36 +70,29 @@ object TestData {
     xmlPayload = xmlPayload)
   val notificationRequestRecord1: NotificationRequestRecord = NotificationRequestRecord(
     notification = notificationRequest1,
-    //TODO make the time set
-    timeReceived = DateTime.now().toDateTimeISO,
+    timeReceived = new DateTime("2018-05-05T10:11:11.123").toDateTimeISO,
     _id = objectId1)
   val notificationRequestRecord2: NotificationRequestRecord = NotificationRequestRecord(
     notification = notificationRequest2,
-    //TODO make the time set
-    timeReceived = DateTime.now().toDateTimeISO,
+    timeReceived = new DateTime("2018-05-05T10:11:12.123").toDateTimeISO,
     _id = objectId2)
   val notificationRequestRecord3: NotificationRequestRecord = NotificationRequestRecord(
     notification = notificationRequest3,
-    //TODO make the time set
-    timeReceived = DateTime.now().toDateTimeISO,
+    timeReceived = new DateTime("2018-05-05T10:11:13.123").toDateTimeISO,
     _id = objectId3)
   val notificationRequestRecord4: NotificationRequestRecord = NotificationRequestRecord(
     notification = notificationRequest1,
-    //TODO make the time set
-    timeReceived = DateTime.now().toDateTimeISO,
+    timeReceived = new DateTime("2018-05-05T10:11:14.123").toDateTimeISO,
     _id = objectId4)
   val notificationRequestRecord5: NotificationRequestRecord = NotificationRequestRecord(
     notification = notificationRequest2,
-    //TODO make the time set
-    timeReceived = DateTime.now().toDateTimeISO,
+    timeReceived = new DateTime("2018-05-05T10:11:15.123").toDateTimeISO,
     _id = objectId5)
   val notificationRequestRecord6: NotificationRequestRecord = NotificationRequestRecord(
     notification = notificationRequest3,
-    //TODO make the time set
-    timeReceived = DateTime.now().toDateTimeISO,
+    timeReceived = new DateTime("2018-05-05T10:11:16.123").toDateTimeISO,
     _id = objectId6)
 
-  //TODO delete these?
   val HeaderOne: (String, String) = "h1" -> "v1"
   val HeaderTwo: (String, String) = "h2" -> "v2"
   val XmlPayload : NodeSeq = <stuff><moreXml>Stuff</moreXml></stuff>
