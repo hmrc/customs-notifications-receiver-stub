25524393e47cef0df96367cb7d0aad90167b4690
Edward Oscar Taylor <48755019+ed1286@users.noreply.github.com>
2023-02-09 11:53:23 +0000

    DCWL-538

--- app/uk/gov/hmrc/customs/notification/receiver/repo/NotificationRequestRecordRepo.scala
+++ app/uk/gov/hmrc/customs/notification/receiver/repo/NotificationRequestRecordRepo.scala
@@ -91,11 +91,11 @@ class NotificationRequestRecordRepo @Inject()(mongoComponent: MongoComponent)(im
     collection.drop().toFuture().map(_ => ())
   }
 
-  def buildCsIdFilter(csId: CsId): conversions.Bson = {
+  private def buildCsIdFilter(csId: CsId): conversions.Bson = {
     equal("notification.csid", csId)
   }
 
-  def buildConversationIdFilter(conversationId: ConversationId): conversions.Bson = {
+  private def buildConversationIdFilter(conversationId: ConversationId): conversions.Bson = {
     equal("notification.conversationId", conversationId)
   }
 
