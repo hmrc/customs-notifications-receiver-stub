9aa7e9cacc6e944897446d4f5b8b65f7f3ee9829
Edward Oscar Taylor <48755019+ed1286@users.noreply.github.com>
2023-06-21 09:17:24 +0100

    DCWL-805-Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,16 @@ import sbt._
 object AppDependencies {
 
   private val testScope = "test,it"
-  private val mongoDbVersion = "0.73.0"
+  private val mongoDbVersion = "1.3.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"                    %% "customs-api-common"   % "1.57.0",
-    "uk.gov.hmrc.mongo"              %% "hmrc-mongo-play-28"   % mongoDbVersion,
-    "com.github.ghik"                %  "silencer-lib"         % "1.7.11"          % Provided cross CrossVersion.full,
-    compilerPlugin("com.github.ghik" %  "silencer-plugin"      % "1.7.11"                     cross CrossVersion.full)
+    "uk.gov.hmrc"                    %% "customs-api-common"   % "1.60.0",
+    "uk.gov.hmrc.mongo"              %% "hmrc-mongo-play-28"   % mongoDbVersion
   )
 
   val test = Seq(
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"         % testScope,
-    "uk.gov.hmrc"            %% "customs-api-common"       % "1.57.0"        % testScope classifier "tests",
+    "uk.gov.hmrc"            %% "customs-api-common"       % "1.60.0"        % testScope classifier "tests",
     "com.github.tomakehurst" %  "wiremock-standalone"      % "2.27.2"        % testScope,
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"      % testScope,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % mongoDbVersion % testScope,
