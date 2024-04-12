47682618850f231f5e612f5719e52a80f65f1340
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-04-12 16:18:40 +0100

    DCWL-2009 formatting

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,13 +7,13 @@ object AppDependencies {
   private val bootstrapVersion = "8.5.0"
 
   val compile = Seq(
-  "uk.gov.hmrc"              %% s"bootstrap-backend$playSuffix" % bootstrapVersion,
-  "uk.gov.hmrc.mongo"        %% s"hmrc-mongo$playSuffix"        % hmrcMongoVersion
+  "uk.gov.hmrc"         %% s"bootstrap-backend$playSuffix" % bootstrapVersion,
+  "uk.gov.hmrc.mongo"   %% s"hmrc-mongo$playSuffix"        % hmrcMongoVersion
   )
 
   val test = Seq(
-    "org.scalatestplus"      %% "mockito-3-4"                 % "3.2.10.0"       ,
-    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test$playSuffix" % hmrcMongoVersion ,
-    "uk.gov.hmrc"            %% s"bootstrap-test$playSuffix"  % bootstrapVersion
+    "org.scalatestplus" %% "mockito-3-4"                   % "3.2.10.0",
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test$playSuffix"   % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% s"bootstrap-test$playSuffix"    % bootstrapVersion
   ).map(_ % Test)
 }
