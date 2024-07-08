70fbf88becaadfb684715f93afe8a5e10f567533
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2024-07-08 15:39:57 +0100

    [DCWL-2372] WIP Notifications Retries 10 times before a success is given

--- app/uk/gov/hmrc/customs/notification/receiver/controllers/CustomsNotificationReceiverController.scala
+++ app/uk/gov/hmrc/customs/notification/receiver/controllers/CustomsNotificationReceiverController.scala
@@ -31,7 +31,9 @@ import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import java.time.{LocalDateTime, ZoneOffset}
 import java.util.UUID
 import javax.inject.Singleton
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
 
 
@@ -41,7 +43,6 @@ class CustomsNotificationReceiverController @Inject()(logger: CdsLogger,
                                                       repo: NotificationRequestRecordRepo,
                                                       cc: ControllerComponents)
                                                      (implicit ec: ExecutionContext) extends BackendController(cc) {
-  val count = scala.collection.mutable.Map[String,String]()
 
   def post(): Action[AnyContent] = Action andThen headerValidationAction async { implicit extractedHeadersRequest =>
     //TODO AS THIS IS MOCKING THE CLIENT WE WOULD LIKE THIS TO RETURN 500 SO MANY TIMES AND THEN RETURN OK
@@ -51,14 +52,50 @@ class CustomsNotificationReceiverController @Inject()(logger: CdsLogger,
         val payloadAsString = xmlPayload.toString
         val notificationRequest = NotificationRequest(extractedHeadersRequest.csid, extractedHeadersRequest.conversationId, extractedHeadersRequest.authHeader, seqOfHeader.toList, payloadAsString)
         logger.debug(s"Received Notification for :[${notificationRequest.csId}]\nheaders=\n[$seqOfHeader]")
-        println(Console.RED_B + Console.BLACK + s"Time: ${LocalDateTime.now()} Payload - FunctionCode: ${notificationRequest.xmlPayload.subSequence(notificationRequest.xmlPayload.indexOf("p:FunctionCode"), notificationRequest.xmlPayload.indexOf("p:FunctionCode") + 20)}" + Console.RESET)
         repo.insertNotificationRequestRecord(NotificationRequestRecord(notificationRequest, LocalDateTime.now(ZoneOffset.UTC), new ObjectId()))
-        if(payloadAsString.contains("failWith-500")){
+        val functionCode: String = notificationRequest.xmlPayload.subSequence(notificationRequest.xmlPayload.indexOf("p:FunctionCode"), notificationRequest.xmlPayload.indexOf("p:FunctionCode") + 20).toString
+        val notificationsReceived = scala.collection.mutable.Map[String, Int]().withDefaultValue(0)
 
-          println(Console.MAGENTA_B + Console.BLACK + s"Time: ${LocalDateTime.now()} Contains LRN: failWith-500" + Console.RESET)
-          Future.successful(InternalServerError(Json.toJson(notificationRequest)))
-        }else
-        Future.successful(Ok(Json.toJson(notificationRequest)))
+
+        def chekPayloadStatus():scala.concurrent.Future[play.api.mvc.Result] = {
+          payloadAsString match {
+            case payloadAsString if payloadAsString.contains("failWith-500") => countTimesReturned()
+            case payloadAsString if payloadAsString.contains("failWith-400") => Future.successful(BadRequest(Json.toJson(notificationRequest))) //TODO RESPONSE FOR 400
+            case _ => Future.successful(Ok(Json.toJson(notificationRequest)))
+          }
+        }
+
+        def countTimesReturned(): scala.concurrent.Future[play.api.mvc.Result] = {
+          functionCode match {
+            case functionCode if functionCode.contains("01") =>
+              println(Console.MAGENTA_B + Console.BLACK + s"Time: ${LocalDateTime.now()} Function Code = 01" + Console.RESET)
+              countResponse(functionCode = "01")
+            case functionCode if functionCode.contains("09") =>
+              println(Console.CYAN_B + Console.BLACK + s"Time: ${LocalDateTime.now()} Function Code = 09" + Console.RESET)
+              countResponse(functionCode = "09")
+            case functionCode if functionCode.contains("13") =>
+              println(Console.GREEN_B + Console.BLACK + s"Time: ${LocalDateTime.now()} Function Code = 13" + Console.RESET)
+              countResponse(functionCode = "13")
+            }
+          }
+
+          def countResponse(functionCode: String): scala.concurrent.Future[play.api.mvc.Result] = {
+            val countNotificationsByCsId = Await.result(repo.countNotificationsByCsId(notificationRequest.csId), 5 seconds)
+            println(Console.GREEN_B + Console.BLACK + s"conversation id count= ${countNotificationsByCsId}" + Console.RESET)
+            val soFar = notificationsReceived(functionCode)
+            println(Console.GREEN_B + Console.BLACK + s"receiveNotification [$functionCode] [$soFar] times so far" + Console.RESET)
+            val now = soFar + 1
+            if (countNotificationsByCsId >= 10) {
+              println(Console.GREEN_B + Console.BLACK + s"RETURN SUCCESS" + Console.RESET)
+              Future.successful(Ok(Json.toJson(notificationRequest)))
+            } else {
+             notificationsReceived += (functionCode -> now)
+             Future.successful(InternalServerError(Json.toJson(notificationRequest)))
+            }
+          }
+
+          chekPayloadStatus()
+
       case None =>
         val message = "Invalid Xml"
         logger.error(message)
